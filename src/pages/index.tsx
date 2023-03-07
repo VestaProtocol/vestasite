809955366120eef22b036d6bfc57df9fb1b57fd7
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-03-07 16:08:55 -0500

    docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { DiGithubBadge } from "react-icons/di";
+import { IoIosJournal } from "react-icons/io"
 import { HiDocument } from "react-icons/hi2";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -31,6 +32,9 @@ export default function Home() {
         <div className='w-fit h-fit flex justify-center'>
           <a className='w-24 h-24' target="_blank" rel="noreferrer" href="https://github.com/VestaProtocol/vesta">
             <DiGithubBadge className='w-full h-full fill-vesta' />
+          </a>
+          <a className='w-20' target="_blank" rel="noreferrer" href="https://docs.vestajs.com">
+            <IoIosJournal className='w-full h-full fill-vesta' />
           </a>
           {/* <a className='w-24 h-24' target="_blank" href="https://github.com/VestaProtocol/vesta">
             <HiDocument className='w-full h-full fill-vesta' />
